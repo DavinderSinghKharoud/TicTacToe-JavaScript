9f4c2a7529c33e546f305660be294ff40cec1131
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-15 10:32:28 -0500

    Added Human Player event listener.

---
+++ script.js
@@ -0,0 +1,45 @@
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[6, 4, 2]
+
+]
+
+//It will select each element in html file which has class name as "cell".
+var cells = document.querySelectorAll('.cell');
+
+startGame();
+
+function startGame(){
+	document.querySelector(".endgame").style.display = "none";
+
+	//Creating array up to number 9
+	origBoard = Array.from(Array(9).keys());
+
+	for(var i = 0; i<cells.length; i++){
+
+			cells[i].innerText = "";
+			cells[i].style.removeProperty('background-color');
+			cells[i].addEventListener('click',turnclick, false);
+	}
+
+}
+
+function turnclick(square){
+	turn(square.target.id, huPlayer);
+	console.log(square.target.id)
+}
+
+function turn(squareId, player){
+
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+}
